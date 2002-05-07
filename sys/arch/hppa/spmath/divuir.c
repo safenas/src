bf6b0671166e579a6d0a5e109fb1e39407add2de
mickey <mickey>
2002-05-07 21:37:20 +0000

    import the original hp sources for the spmath library w/ the a bsd-like hp licensing on 'em. w/ many thanks to: Mike Hibler <mike@fast.cs.utah.edu> James Loveluck <loveluck@ri.silicomp.fr> Patrick Roudaud <patrick@enserg.fr>

---
+++ sys/arch/hppa/spmath/divuir.c
@@ -0,0 +1,38 @@
+/*
+  (c) Copyright 1986 HEWLETT-PACKARD COMPANY
+  To anyone who acknowledges that this file is provided "AS IS"
+  without any express or implied warranty:
+      permission to use, copy, modify, and distribute this file
+  for any purpose is hereby granted without fee, provided that
+  the above copyright notice and this notice appears in all
+  copies, and that the name of Hewlett-Packard Company not be
+  used in advertising or publicity pertaining to distribution
+  of the software without specific, written prior permission.
+  Hewlett-Packard Company makes no representations about the
+  suitability of this software for any purpose.
+*/
+/* $Source: /home/cvs/src/sys/arch/hppa/spmath/divuir.c,v $
+ * $Revision: 1.1.1.1 $	$Author: mickey $
+ * $State: Exp $   	$Locker:  $
+ * $Date: 2002/05/07 21:37:19 $
+ */
+
+
+#include "../spmath/md.h"
+
+VOID divuir(opnd1,opnd2,result)
+
+unsigned int opnd1, opnd2;
+struct mdsfu_register *result;
+{
+	/* get source operands, check divisor for zero */
+	if (opnd2 == 0) {
+		overflow = TRUE;
+		return;
+	}
+	overflow = FALSE;
+
+	/* do the divide */
+	divu(0,opnd1,opnd2,result);
+	return;
+}
---
+++ sys/arch/hppa/spmath/mpyscv.c
@@ -0,0 +1,31 @@
+/*
+  (c) Copyright 1986 HEWLETT-PACKARD COMPANY
+  To anyone who acknowledges that this file is provided "AS IS"
+  without any express or implied warranty:
+      permission to use, copy, modify, and distribute this file
+  for any purpose is hereby granted without fee, provided that
+  the above copyright notice and this notice appears in all
+  copies, and that the name of Hewlett-Packard Company not be
+  used in advertising or publicity pertaining to distribution
+  of the software without specific, written prior permission.
+  Hewlett-Packard Company makes no representations about the
+  suitability of this software for any purpose.
+*/
+/* $Source: /home/cvs/src/sys/arch/hppa/spmath/mpyscv.c,v $
+ * $Revision: 1.1.1.1 $	$Author: mickey $
+ * $State: Exp $   	$Locker:  $
+ * $Date: 2002/05/07 21:37:19 $
+ */
+
+
+#include "../spmath/md.h"
+
+VOID mpyscv(opnd1,opnd2,result)
+
+int opnd1, opnd2;
+struct mdsfu_register *result;
+{
+	impys(&opnd1,&opnd2,result);
+	overflow = FALSE;
+	return;
+}
