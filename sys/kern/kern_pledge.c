0e4f9fc9409163b8d7e8434a11f466b3f9849aa6
bluhm <bluhm>
2025-06-29 13:58:43 +0000

    Avoid NULL pointer dereference when TIOCUCNTL ioctl is called with a non-file argument.
    from jsg@; feedback and ok deraadt@
    
    this is errata/7.7/008_pledge.patch.sig

--- sys/kern/kern_pledge.c
+++ sys/kern/kern_pledge.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_pledge.c,v 1.322 2025/02/10 16:45:46 deraadt Exp $	*/
+/*	$OpenBSD: kern_pledge.c,v 1.323 2025/02/12 14:11:26 deraadt Exp $	*/
 
 /*
  * Copyright (c) 2015 Nicholas Marriott <nicm@openbsd.org>
@@ -1263,9 +1263,11 @@ pledge_ioctl(struct proc *p, long com, struct file *fp)
 				break;
 			if ((pledge & PLEDGE_WPATH) == 0)
 				break;
-			if (cdevsw[major(vp->v_rdev)].d_open != ptcopen)
-				break;
-			return (0);
+			if (fp->f_type == DTYPE_VNODE &&
+			    vp->v_type == VCHR &&
+			    cdevsw[major(vp->v_rdev)].d_open == ptcopen)
+				return (0);
+			break;
 #endif /* NPTY > 0 */
 		case TIOCSPGRP:
 			if ((pledge & PLEDGE_PROC) == 0)
